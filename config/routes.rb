e4f99de2ffd7e54ab67fb5f0041e0573f2a2e1ba
Tony Buser <tbuser@gmail.com>
2010-08-27 01:01:53 -0400

    javascript STL parser mostly functional

--- config/routes.rb
+++ config/routes.rb
@@ -50,4 +50,6 @@ ActionController::Routing::Routes.draw do |map|
   # consider removing or commenting them out if you're using named routes and resources.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
+  
+  map.connect ':action', :controller => "pages"
 end
