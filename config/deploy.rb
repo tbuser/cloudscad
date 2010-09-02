874451410cefeb777d9f83d77f2defc5d6cefb91
Tony Buser <tbuser@gmail.com>
2010-09-02 01:14:27 -0400

    this has to work

--- config/deploy.rb
+++ config/deploy.rb
@@ -75,28 +75,10 @@ end
 #   end
 # end
 
-namespace :bundler do
-  # task :install, :roles => :app, :except => { :no_release => true }  do
-  #   run("gem install bundler --source=http://gemcutter.org")
-  # end
-  # 
-  # task :symlink_vendor, :roles => :app, :except => { :no_release => true } do
-  #   shared_gems = File.join(shared_path, 'vendor/gems/ruby/1.8')
-  #   release_gems = "#{release_path}/vendor/gems/ruby/1.8"
-  #   # if you don't commit your cache, add cache to this list
-  #   %w(gems specifications).each do |sub_dir|
-  #     shared_sub_dir = File.join(shared_gems, sub_dir)
-  #     run("mkdir -p #{shared_sub_dir} && mkdir -p #{release_gems} && ln -s #{shared_sub_dir} #{release_gems}/#{sub_dir}")
-  #   end
-  # end
-
-  task :bundle_new_release, :roles => :app, :except => { :no_release => true }  do
-    # bundler.symlink_vendor
-    # if you don't commit your cache, remove --cached from this line
-    # run("cd #{release_path} && #{try_sudo} gem bundle --only #{rails_env} --cached")
-    rails_env = variables[:rails_env] || 'production'
-    run("cd #{release_path} && #{try_sudo} bundle install --only #{rails_env}")
+namespace :bundle do
+  task :install, :roles => :app, :except => { :no_release => true }  do
+    run("cd #{release_path} && #{try_sudo} bundle install --deployment")
   end
 end
 
-after 'deploy:update_code', 'bundler:bundle_new_release'
+after 'deploy:update_code', 'bundle:install'
